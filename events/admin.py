f5d0eed3abf0950b78170a659734526a5a75845a
unknown <slokesh1810@gmail.com>
2025-07-19 11:10:52 +0530

    Admin Approval for providers created

--- events/admin.py
+++ events/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
-from .models import Event, profile,Participant
+from django.contrib.auth.models import User
+from .models import Event,profile,Participant
 
 # Register your models here.
 admin.site.register(Event)
 admin.site.register(profile)
 admin.site.register(Participant)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'role', 'is_approved')
+    list_filter = ('is_approved')
+    actions = ['approve_providers']
+
+    def approve_providers(self, request, queryset):
+        updated = queryset.update(is_approved=True)
+        self.message_user(request, f"{updated} provider(s) approved.")
+
+def ready(self):
+    import signals
--- events/models.py
+++ events/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=10, choices=(('participant','Participant'),('provider','Provider'),))
+    is_approved = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.user} ({self.role})"
